d3d29b693a2107ea0c53e9fe2c39a9482fb55a01
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2026-09-11 01:30:51 +0400

    Fixing Searchbar

--- src/shared/SearchBar.tsx
+++ src/shared/SearchBar.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Fuse from 'fuse.js';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 interface SearchItem {
     slug: string;
